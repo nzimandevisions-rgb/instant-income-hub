f4de75a81ce31693b02971a7ebfb7480ecdcf44f
nzimandevisions-rgb <nzimandevisions@gmail.com>
2026-09-01 22:13:43 +0200

    Delete src/start.ts

--- src/start.ts
+++
@@ -1,7 +0,0 @@
-import { createStartHandler, defaultStreamHandler } from "@tanstack/react-start/server";
-import { getRouter } from "./router";
-
-export const startInstance = createStartHandler({
-  createRouter: getRouter,
-  streamHandler: defaultStreamHandler,
-});
